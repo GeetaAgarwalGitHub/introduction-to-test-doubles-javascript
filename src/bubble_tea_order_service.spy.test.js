26d62e447128bc33d0913af1f78d4560d5a49ba3
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-03-11 12:29:52 +0000

    Update bubble_tea_order_service.spy.test.js
    Implemented parameterised testing

--- src/bubble_tea_order_service.spy.test.js
+++ src/bubble_tea_order_service.spy.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable*/
 const {createOrderRequest} = require('./bubble_tea_order_service');
 const bubbleTeaType = require('./bubble_tea_type');
 const messenger = require('./bubble_tea_messenger');
@@ -36,4 +37,32 @@ test('test successful bubble tea order request when using a spy', () => {
   expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
   expect(emailSpy).toHaveBeenCalledWith(orderRequest);
   expect(emailSpy).toHaveBeenCalledTimes(1);
+});
+
+
+describe("This is a parameterised test; should generate  bubble tea based on input provided", () => {
+  test.each`
+  bubbleTeaType
+  ${"JASMINEMILKTEA"}
+  ${"OOLONGMILKTEA"}
+  ${"MATCHAMILKTEA"}
+  ${"PEACHICETEA"}
+  ${"LYCHEEICETEA"}
+`('test successful for $bubbleTeaType bubble tea order request when using a spy', ({bubbleTeaType}) => {
+  // Arrange
+  const bubbleTeaRequest = {
+    paymentDetails: dummyPaymentDetails,
+    bubbleTea: {
+      type: bubbleTeaType,
+    },
+  };
+  // Act
+  const orderRequest = createOrderRequest(bubbleTeaRequest);
+
+  // Assert
+  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
+  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
+  expect(emailSpy).toHaveBeenCalledWith(orderRequest);
+  expect(emailSpy).toHaveBeenCalledTimes(1);
+});
 });
