7b8859204c8bef7cca1937c0ff3cc7dc201e059b
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-03-11 12:30:03 +0000

    Update bubble_tea_roulette_service.test.js
    Implemented parameterised testing

--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -1,16 +1,37 @@
-const getRandomBubbleTeaType = require('./bubble_tea_roulette_service');
+const getRandomBubbleTeaType = require("./bubble_tea_roulette_service");
 
-test('should generate random bubble tea', () => {
+test("should generate random bubble tea", () => {
   // Arrange
   // Stub to calls to Math.random to return 0.2
-  jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
+  jest.spyOn(global.Math, "random").mockReturnValue(0.2);
 
   // Act
   const bubbleTea = getRandomBubbleTeaType();
 
   // Assert
-  expect(bubbleTea).toBe('JASMINEMILKTEA');
+  expect(bubbleTea).toBe("JASMINEMILKTEA");
 
   // Restore the default Math.random
-  jest.spyOn(global.Math, 'random').mockRestore();
+  jest.spyOn(global.Math, "random").mockRestore();
+});
+
+describe("This is a parameterised test; should generate  bubble tea based on input provided", () => {
+  it.each([
+    [0, "OOLONGMILKTEA"],
+    [0.2, "JASMINEMILKTEA"],
+    [0.4, "MATCHAMILKTEA"],
+    [0.6, "PEACHICETEA"],
+    [0.8, "LYCHEEICETEA"],
+  ])("Passing a value of  %p - Expecting : %p", (randomValue, result) => {
+    jest.spyOn(global.Math, "random").mockReturnValue(randomValue);
+
+    // Act
+    const bubbleTea = getRandomBubbleTeaType();
+
+    // Assert
+    expect(bubbleTea).toBe(result);
+
+    // Restore the default Math.random
+    jest.spyOn(global.Math, "random").mockRestore();
+  });
 });
